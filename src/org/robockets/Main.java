91313d3f44457437814b4f2cf881d90bb558c195
Jake Backer <jbacker42@gmail.com>
2016-12-02 09:18:43 -0500

    Add wait and stop methods

--- src/org/robockets/Main.java
+++ src/org/robockets/Main.java
@@ -1,7 +1,48 @@
 package org.robockets;
 
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class Main {
+
+    static NetworkTable netTable;
+    static Robot keyBot;
+
     public static void main(String[] args) {
-        
+        netTable = NetworkTable.getTable("AutoPID");
+        try {
+            keyBot = new Robot();
+            waitForStart();
+            stop(); // For testing
+        } catch (InterruptedException | AWTException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void waitForStart() throws InterruptedException{
+        while (netTable.getBoolean("IsStarted", false)) {
+            System.out.println("Waiting...");
+            Thread.sleep(100);
+        }
+
+        System.out.println("Running!");
+        System.out.println("Starting...");
+    }
+
+    public static void stop() throws AWTException, InterruptedException{
+        stop(0, false);
+    }
+
+    public static void stop(double seconds, boolean emergency) throws AWTException, InterruptedException{
+        Thread.sleep((long)seconds*100);
+        for (int i=0; i<5;i++) {
+            if (emergency) {
+                keyBot.keyPress(KeyEvent.VK_SPACE);
+            } else {
+                keyBot.keyPress(KeyEvent.VK_ENTER);
+            }
+        }
     }
 }
